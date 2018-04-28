61bafbfed6f2843457f6fd904a148c19b108ece2
jrschellenberg <jrschellenberg@gmail.com>
2018-04-28 05:58:06 +0000

    Multiple bug fixes, change docker format

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-CONTAINER_NAME=explorer
+CONTAINER_NAME=shekel
+DB_CONTAINER_NAME=mongoShekel
 # deploy Script
 export CONTAINER_NAME=$CONTAINER_NAME
+export DB_CONTAINER_NAME=$DB_CONTAINER_NAME
 export PORT=3001
 docker-compose -p $CONTAINER_NAME up --build
