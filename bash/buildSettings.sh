3ecf6ca9100e8e1da39b84aac903601592ef6a4c
Justin Schellenberg <jrschellenberg@gmail.com>
2018-05-02 15:15:13 -0500

    fixed variable declaration issues

--- bash/buildSettings.sh
+++ bash/buildSettings.sh
@@ -1,103 +1,104 @@
 #!/bin/bash
-rm "$WORKING_PATH/settings.json"
+WRITE_PATH=${WORKING_PATH}/settings.json
+rm ${WRITE_PATH}
 
-echo '{' >> "$WORKING_PATH/settings.json"
-echo '  "title": "$COIN Block Explorer",' >> "$WORKING_PATH/settings.json"
-echo '  "address": "localhost:$PORT",' >> "$WORKING_PATH/settings.json"
-echo '  "coin": "$COIN",' >> "$WORKING_PATH/settings.json"
-echo '  "symbol": "$SYMBOL",' >> "$WORKING_PATH/settings.json"
-echo '  "logo": "/images/logo.png",' >> "$WORKING_PATH/settings.json"
-echo '  "favicon": "public/favicon.ico",' >> "$WORKING_PATH/settings.json"
-echo '  "theme": "$THEME",' >> "$WORKING_PATH/settings.json"
-echo '  "port" : $PORT,' >> "$WORKING_PATH/settings.json"
-echo '  "dbsettings": {' >> "$WORKING_PATH/settings.json"
-echo '    "user": "$DB_USER",' >> "$WORKING_PATH/settings.json"
-echo '    "password": "$DB_PASSWORD",' >> "$WORKING_PATH/settings.json"
-echo '    "database": "$DB_DATABASE",' >> "$WORKING_PATH/settings.json"
-echo '    "address": "$DB_CONTAINER_NAME",' >> "$WORKING_PATH/settings.json"
-echo '    "port": 27017' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '{' >> ${WRITE_PATH}
+echo '  "title": "${COIN} Block Explorer",' >> ${WRITE_PATH}
+echo '  "address": "localhost:${PORT}",' >> ${WRITE_PATH}
+echo '  "coin": "${COIN}",' >> ${WRITE_PATH}
+echo '  "symbol": "${SYMBOL}",' >> ${WRITE_PATH}
+echo '  "logo": "/images/logo.png",' >> ${WRITE_PATH}
+echo '  "favicon": "public/favicon.ico",' >> ${WRITE_PATH}
+echo '  "theme": "${THEME}",' >> ${WRITE_PATH}
+echo '  "port" : ${PORT},' >> ${WRITE_PATH}
+echo '  "dbsettings": {' >> ${WRITE_PATH}
+echo '    "user": "${DB_USER}",' >> ${WRITE_PATH}
+echo '    "password": "${DB_PASSWORD}",' >> ${WRITE_PATH}
+echo '    "database": "${DB_DATABASE}",' >> ${WRITE_PATH}
+echo '    "address": "${DB_CONTAINER_NAME}",' >> ${WRITE_PATH}
+echo '    "port": 27017' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "update_timeout": 10,' >> "$WORKING_PATH/settings.json"
-echo '  "check_timeout": 250,' >> "$WORKING_PATH/settings.json"
+echo '  "update_timeout": 10,' >> ${WRITE_PATH}
+echo '  "check_timeout": 250,' >> ${WRITE_PATH}
 
-echo '  "wallet": {' >> "$WORKING_PATH/settings.json"
-echo '    "host": "localhost",' >> "$WORKING_PATH/settings.json"
-echo '    "port": $WALLET_PORT,' >> "$WORKING_PATH/settings.json"
-echo '    "user": "$WALLET_USER",' >> "$WORKING_PATH/settings.json"
-echo '    "pass": "$WALLET_PASSWORD"' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "wallet": {' >> ${WRITE_PATH}
+echo '    "host": "localhost",' >> ${WRITE_PATH}
+echo '    "port": ${WALLET_PORT},' >> ${WRITE_PATH}
+echo '    "user": "${WALLET_USER}",' >> ${WRITE_PATH}
+echo '    "pass": "${WALLET_PASSWORD}"' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "confirmations": 40,' >> "$WORKING_PATH/settings.json"
-echo '  "locale": "locale/en.json",' >> "$WORKING_PATH/settings.json"
+echo '  "confirmations": 40,' >> ${WRITE_PATH}
+echo '  "locale": "locale/en.json",' >> ${WRITE_PATH}
 
-echo '  "display": {' >> "$WORKING_PATH/settings.json"
-echo '    "api": true,' >> "$WORKING_PATH/settings.json"
-echo '    "markets": false,' >> "$WORKING_PATH/settings.json"
-echo '    "richlist": true,' >> "$WORKING_PATH/settings.json"
-echo '    "twitter": false,' >> "$WORKING_PATH/settings.json"
-echo '    "facebook": false,' >> "$WORKING_PATH/settings.json"
-echo '    "googleplus": false,' >> "$WORKING_PATH/settings.json"
-echo '    "search": true,' >> "$WORKING_PATH/settings.json"
-echo '    "movement": true,' >> "$WORKING_PATH/settings.json"
-echo '    "network": true' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "display": {' >> ${WRITE_PATH}
+echo '    "api": true,' >> ${WRITE_PATH}
+echo '    "markets": false,' >> ${WRITE_PATH}
+echo '    "richlist": true,' >> ${WRITE_PATH}
+echo '    "twitter": false,' >> ${WRITE_PATH}
+echo '    "facebook": false,' >> ${WRITE_PATH}
+echo '    "googleplus": false,' >> ${WRITE_PATH}
+echo '    "search": true,' >> ${WRITE_PATH}
+echo '    "movement": true,' >> ${WRITE_PATH}
+echo '    "network": true' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "index": {' >> "$WORKING_PATH/settings.json"
-echo '    "show_hashrate": true,' >> "$WORKING_PATH/settings.json"
-echo '    "difficulty": "POW",' >> "$WORKING_PATH/settings.json"
-echo '    "last_txs": 100' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "index": {' >> ${WRITE_PATH}
+echo '    "show_hashrate": true,' >> ${WRITE_PATH}
+echo '    "difficulty": "POW",' >> ${WRITE_PATH}
+echo '    "last_txs": 100' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "api": {' >> "$WORKING_PATH/settings.json"
-echo '    "blockindex": $BLOCKINDEX,' >> "$WORKING_PATH/settings.json"
-echo '    "blockhash": "$BLOCKHASH",' >> "$WORKING_PATH/settings.json"
-echo '    "txhash": "$TXHASH",' >> "$WORKING_PATH/settings.json"
-echo '    "address": "$ADDRESS"' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "api": {' >> ${WRITE_PATH}
+echo '    "blockindex": ${BLOCKINDEX},' >> ${WRITE_PATH}
+echo '    "blockhash": "${BLOCKHASH}",' >> ${WRITE_PATH}
+echo '    "txhash": "${TXHASH}",' >> ${WRITE_PATH}
+echo '    "address": "${ADDRESS}"' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "markets": {' >> "$WORKING_PATH/settings.json"
-echo '    "coin": "JBS",' >> "$WORKING_PATH/settings.json"
-echo '    "exchange": "BTC",' >> "$WORKING_PATH/settings.json"
-echo '    "enabled": ["bittrex"],' >> "$WORKING_PATH/settings.json"
-echo '    "cryptopia_id": "1658",' >> "$WORKING_PATH/settings.json"
-echo '    "ccex_key" : "Get-Your-Own-Key",' >> "$WORKING_PATH/settings.json"
-echo '    "default": "bittrex"' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "markets": {' >> ${WRITE_PATH}
+echo '    "coin": "JBS",' >> ${WRITE_PATH}
+echo '    "exchange": "BTC",' >> ${WRITE_PATH}
+echo '    "enabled": ["bittrex"],' >> ${WRITE_PATH}
+echo '    "cryptopia_id": "1658",' >> ${WRITE_PATH}
+echo '    "ccex_key" : "Get-Your-Own-Key",' >> ${WRITE_PATH}
+echo '    "default": "bittrex"' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "richlist": {' >> "$WORKING_PATH/settings.json"
-echo '    "distribution": true,' >> "$WORKING_PATH/settings.json"
-echo '    "received": true,' >> "$WORKING_PATH/settings.json"
-echo '    "balance": true' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "richlist": {' >> ${WRITE_PATH}
+echo '    "distribution": true,' >> ${WRITE_PATH}
+echo '    "received": true,' >> ${WRITE_PATH}
+echo '    "balance": true' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "movement": {' >> "$WORKING_PATH/settings.json"
-echo '    "min_amount": 300,' >> "$WORKING_PATH/settings.json"
-echo '    "low_flag": 3000,' >> "$WORKING_PATH/settings.json"
-echo '    "high_flag": 10000' >> "$WORKING_PATH/settings.json"
-echo '  },' >> "$WORKING_PATH/settings.json"
+echo '  "movement": {' >> ${WRITE_PATH}
+echo '    "min_amount": 300,' >> ${WRITE_PATH}
+echo '    "low_flag": 3000,' >> ${WRITE_PATH}
+echo '    "high_flag": 10000' >> ${WRITE_PATH}
+echo '  },' >> ${WRITE_PATH}
 
-echo '  "twitter": "iquidus",' >> "$WORKING_PATH/settings.json"
-echo '  "facebook": "yourfacebookpage",' >> "$WORKING_PATH/settings.json"
-echo '  "googleplus": "yourgooglepluspage",' >> "$WORKING_PATH/settings.json"
+echo '  "twitter": "iquidus",' >> ${WRITE_PATH}
+echo '  "facebook": "yourfacebookpage",' >> ${WRITE_PATH}
+echo '  "googleplus": "yourgooglepluspage",' >> ${WRITE_PATH}
 
-echo '  "genesis_tx": "$GENESIS_TX",' >> "$WORKING_PATH/settings.json"
-echo '  "genesis_block": "$GENESIS_BLOCK",' >> "$WORKING_PATH/settings.json"
+echo '  "genesis_tx": "${GENESIS_TX}",' >> ${WRITE_PATH}
+echo '  "genesis_block": "${GENESIS_BLOCK}",' >> ${WRITE_PATH}
 
-echo '  "heavy": false,' >> "$WORKING_PATH/settings.json"
+echo '  "heavy": false,' >> ${WRITE_PATH}
 
-echo '  "txcount": 100,' >> "$WORKING_PATH/settings.json"
+echo '  "txcount": 100,' >> ${WRITE_PATH}
 
-echo '  "show_sent_received": true,' >> "$WORKING_PATH/settings.json"
+echo '  "show_sent_received": true,' >> ${WRITE_PATH}
 
-echo '  "supply": "COINBASE",' >> "$WORKING_PATH/settings.json"
+echo '  "supply": "COINBASE",' >> ${WRITE_PATH}
 
-echo '  "nethash": "getnetworkhashps",' >> "$WORKING_PATH/settings.json"
+echo '  "nethash": "getnetworkhashps",' >> ${WRITE_PATH}
 
-echo '  "nethash_units": "M",' >> "$WORKING_PATH/settings.json"
+echo '  "nethash_units": "M",' >> ${WRITE_PATH}
 
-echo '  "labels": {' >> "$WORKING_PATH/settings.json"
-echo '  }' >> "$WORKING_PATH/settings.json"
-echo '}' >> "$WORKING_PATH/settings.json"
+echo '  "labels": {' >> ${WRITE_PATH}
+echo '  }' >> ${WRITE_PATH}
+echo '}' >> ${WRITE_PATH}
 
 
